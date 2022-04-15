376c40cd358a1e367672996aab80ce29db0480d1
Yossef-Dawoad <yossefdawoad15@gmail.com>
2022-04-15 16:34:23 +0200

    add csrf trusted origin

--- project/settings.py
+++ project/settings.py
@@ -38,9 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'pages',
     'attendance',
-    "corsheaders",
+
 
 ]
 
@@ -48,12 +49,12 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
-    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     # 'project.Middleware.RedirectMiddleware',
 ]
 
@@ -154,3 +155,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOW_ALL_ORIGINS = True
 DEBUG_PROPAGATE_EXCEPTIONS = True
+CSRF_TRUSTED_ORIGINS = ["https://eyeplus.herokuapp.com"]
